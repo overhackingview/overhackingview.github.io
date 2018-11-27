7d8bd5c98131d105d7f9589c584f2d5e1535c2e8
Jonatas Emidio <jonatasemidio@gmail.com>
2018-11-27 21:40:15 -0200

    Update _mailChimp.js

--- _assets/js/components/_mailChimp.js
+++ _assets/js/components/_mailChimp.js
@@ -24,6 +24,37 @@ const MailChimp = (() => {
       s = this.settings();
       this.bindEvents();
     },
+
+    bindEvents() {
+      s.formSubmit.on('click', e => {
+        e.preventDefault();
+
+        $.ajax({
+          url: s.formAction,
+          type: 'POST',
+          data: s.form.serialize(),
+          dataType: 'jsonp',
+          success: response => {
+            if (response.result === 'error') {
+              setTimeout(() => {
+                s.formMessage.text(`${response.msg}.`);
+                s.formMessage.removeClass('hidden');
+                s.formMessage.addClass(s.animation);
+              }, 750);
+            } else {
+              window.location = `http://${top.location.host.toString()}/subscribe`;
+            }
+          },
+          error: () => {
+            setTimeout(() => {
+              s.formMessage.text('There was an error.');
+              s.formMessage.removeClass('hidden');
+              s.formMessage.addClass(s.animation);
+            }, 750);
+          }
+        });
+      });
+    }
   };
 })();
 
